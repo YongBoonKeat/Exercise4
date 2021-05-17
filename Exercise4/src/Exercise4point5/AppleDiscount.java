cc4c77c33b830ccf012822fe1f0d96d8d101eefe
LEGION <LEGION@LAPTOP-DHOK1Q7Q>
2021-05-17 15:40:56 +0800

    Exercise 4.45

---
+++ Exercise4/src/Exercise4point5/AppleDiscount.java
@@ -0,0 +1,20 @@
+package Exercise4point5;
+
+class AppleDiscount implements Discount{
+		public double discountRate() {
+			return 0.05;//5% discount
+		}
+	}
+
+class MangoDiscount implements Discount{
+	public double discountRate() {
+		return 0.02;//2% discount
+	}
+}
+
+class DragonFruitDiscount implements Discount{
+		public double discountRate() {
+			return 0.03;//3% discount
+		}
+	}
+
